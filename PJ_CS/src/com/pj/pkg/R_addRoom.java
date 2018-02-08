182249b029bcd767b18767b88198375b0b362d87
COMSCI <COMSCI@172.25.4.163>
2018-02-08 23:47:07 +0700

    edit db

---
+++ PJ_CS/src/com/pj/pkg/R_addRoom.java
@@ -0,0 +1,43 @@
+package com.pj.pkg;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class R_addRoom {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					R_addRoom window = new R_addRoom();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public R_addRoom() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
